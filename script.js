722a4c5189b99c3763c57e203e328d5a4c966621
Mayrahelena <may.helenaoliveira@gmail.com>
2024-06-25 22:59:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const btnDownloadCurriculo = document.getElementById('btnDownloadCurriculo');
+
+btnDownloadCurriculo.addEventListener('click', function() {
+  window.open('https://1drv.ms/b/c/dd9d6956ad4efbf3/ERUT1bP1FsZFnMQK7HBB7dcB7O_IUzwFec4c9nePcDe-WA?e=yD4XZy', '_blank'); // Substitua por URL do seu PDF
+});
